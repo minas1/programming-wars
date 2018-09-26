bca4e3aad895722e29ac24bc147da8b85be28284
Minas Mina <minasm1990@gmail.com>
2018-09-26 17:35:04 +0300

    Grid is drawn.

--- core/src/greenbits/programmingwars/Game.java
+++ core/src/greenbits/programmingwars/Game.java
@@ -2,32 +2,122 @@ package greenbits.programmingwars;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Game extends ApplicationAdapter {
+
+    private static final float MIN_VIEWPORT_DIMENSION = 100f;
+
 	SpriteBatch batch;
 	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+
+    private ShapeRenderer shapeRenderer;
+
+    private Camera camera;
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create() {
+
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+
+        shapeRenderer = new ShapeRenderer();
+
+        camera = new OrthographicCamera(calculateViewportWidth(), calculateViewportHeight());
+        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+        camera.update();
 	}
+
+	private float calculateViewportWidth() {
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        if (w > h) {
+            return MIN_VIEWPORT_DIMENSION * (w / h);
+        }
+        else {
+            return MIN_VIEWPORT_DIMENSION;
+        }
+    }
+
+    private float calculateViewportHeight() {
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        if (h > w) {
+            return MIN_VIEWPORT_DIMENSION * (h / w);
+        }
+        else {
+            return MIN_VIEWPORT_DIMENSION;
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+        camera.viewportWidth = calculateViewportWidth();
+        camera.viewportHeight = calculateViewportHeight();
+        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+        camera.update();
+    }
+
+    @Override
+	public void render() {
+
+	    camera.update();
+
+	    Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        float cellDimensions = (MIN_VIEWPORT_DIMENSION - 2f) / GRID_SIZE;
+        drawGrid(1f, 1f, cellDimensions);
+
+//		batch.begin();
+////		batch.draw(img, 0, 0);
+//		batch.end();
+	}
+
+	private static final int GRID_SIZE = 20;
+
+	private void drawGrid(float x, float y, float cellDimensions) {
+
+	    shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        // horizontal
+//        System.out.println("--------");
+        for (int i = 0; i < GRID_SIZE + 1; ++i) {
+
+            float lineY = y + i * cellDimensions;
+
+//            System.out.println("y = " + lineY);
+
+            shapeRenderer.line(x, lineY, x + GRID_SIZE * cellDimensions, lineY);
+        }
+//        System.out.println("--------");
+
+        // vertical
+        for (int i = 0; i < GRID_SIZE + 1; ++i) {
+
+            float lineX = x + i * cellDimensions;
+            shapeRenderer.line(lineX, y, lineX, y + GRID_SIZE * cellDimensions);
+        }
+
+        shapeRenderer.end();
+    }
 	
 	@Override
-	public void dispose () {
+	public void dispose() {
+
 		batch.dispose();
 		img.dispose();
+		shapeRenderer.dispose();
 	}
 }
--- desktop/src/greenbits/programmingwars/desktop/DesktopLauncher.java
+++ desktop/src/greenbits/programmingwars/desktop/DesktopLauncher.java
@@ -2,13 +2,16 @@ package greenbits.programmingwars.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import greenbits.programmingwars.Game;
 
 public class DesktopLauncher {
 
 	public static void main (String[] arg) {
 
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new Game(), config);
 	}
 }
